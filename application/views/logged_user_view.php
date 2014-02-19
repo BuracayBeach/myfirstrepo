9901b330e215eb263fd6db70270d4e049d919ba9
cjqvita <cjqvita@gmail.com>
2014-02-20 01:22:46 +0800

    Updated logged_use_view.php

--- application/views/logged_user_view.php
+++ application/views/logged_user_view.php
@@ -2,7 +2,7 @@ Hi <?php echo $_SESSION['username']; ?> </br>
 
 <ul>
 	<li>
-		<a href="<?php echo base_url();?>index.php/user_account/getData">Update your profile</a>
+		<a href="<?php echo base_url();?>index.php/user_account/update_account">Update your profile</a>
 	</li>
 	<li>
 		<a href="<?php echo base_url();?>index.php/user_account/logout">Log-out</a>
