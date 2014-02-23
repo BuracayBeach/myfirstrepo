ec0ef314261ac4e50b3d78074cb412896114b0df
Allan Conda <allan.conda@gmail.com>
2014-02-23 11:19:43 +0800

    - added faqing files

--- application/views/faq_view.php
+++ application/views/faq_view.php
@@ -6,10 +6,21 @@
  * Time: 1:01 PM
  */
 ?>
-
+<br/><br/><br/><br/><br/>
 <div id="faq_container">
+    <button id="faq_button">Add a FAQ</button>
     <div id="faq_table_container">
 
+        <table id="faq_table">
+            <tbody>
+            <tr id="">
+                <td class="">
+    					<span class="question">
+    					<span class="answer">
+                </td>
+            </tr>
+            </tbody>
+        </table>
     </div>
     <!--script src="<?php echo base_url() ?>js/faq_table_generator.js"></script-->
 </div>
