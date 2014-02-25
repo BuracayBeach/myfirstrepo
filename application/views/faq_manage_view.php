030303e4f7c3318b30f95c9497ccecfe068b0a39
Allan Conda <allan.conda@gmail.com>
2014-02-25 13:25:32 +0800

    -renamed nicEdit src to compressed file

--- application/views/faq_manage_view.php
+++ application/views/faq_manage_view.php
@@ -47,6 +47,6 @@
         </table>
     </div>
 </div>
-<script src="<?php echo base_url() ?>js/nicEdit_full.js"></script>
+<script src="<?php echo base_url() ?>js/nicEdit.js"></script>
 <script src="<?php echo base_url() ?>js/faq_table_generator.js"></script>
 <script src="<?php echo base_url(); ?>js/faq_manager.js"></script>
