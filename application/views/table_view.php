e36263d2d3b9409669c41f9e2ecc02483b5db591
Kevin Lloyd Bernal <kevinoxy@gmail.com>
2014-03-02 22:03:59 +0800

    fix CSS search view

--- application/views/table_view.php
+++ application/views/table_view.php
@@ -1,14 +1,15 @@
-<?php include 'pagination_view.php';?>
 
 <div id="search_table_container" class="small-6 column">
+    <?php include 'pagination_view.php';?>
+
     <table id="search_table" border="1">
         <?php
 
             if(isset($table) && isset($page)){
                 echo "<span id='search_results_label'>";
                 if (trim($search_term)=='') echo "View all Books";
-                else  echo "Search Results for  '" . htmlspecialchars(stripslashes(trim($search_term))) . "'";
-                echo "</span><br/><br/>";
+                else  echo "Search Results for  '<span class='bold'>" . htmlspecialchars(stripslashes(trim($search_term))) . "</span>'";
+                echo "</span>";
 
                 echo "<tr >
                     <th width='15%'>Identification</th>
@@ -30,8 +31,10 @@
                 $row_max = ($page)*$rows_per_page - 1;
                 if ($row_max > $total_count - 1) $row_max = $total_count - 1;
 
+                echo "<div class='search_results_counter'>";
                 echo $row_min+1 . "-";
-                echo $row_max+1 . " of $total_count<br>";
+                echo $row_max+1 . " of $total_count";
+                echo "<div><br>";
         
                 for($a=$row_min ; $a<=$row_max ; $a++){
                     if (!isset($table[$a])) break;
@@ -45,9 +48,10 @@
 
         ?>
     </table>
+
+    <?php include 'pagination_view.php';?>
 </div>
 
-<?php include 'pagination_view.php';?>
 
 
 
