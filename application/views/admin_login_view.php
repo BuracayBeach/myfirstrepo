d5d08130f03d76f995729b18dfc43df4b36fdfde
Kevin Lloyd Bernal <kevinoxy@gmail.com>
2014-03-02 20:05:40 +0800

    fixed CSS notifs, styled admin login

--- application/views/admin_login_view.php
+++ application/views/admin_login_view.php
@@ -1,5 +1,25 @@
-<form action = "<?php echo base_url();?>index.php/admin_account/admin_login" method = "post">
-	<input type = "text" name="username" placeholder="username"/>
-	<input type = "password" name="password" placeholder="password"/>
-	<input type = "submit" name="submit" value="Login"/>
-</form>
+<link rel="stylesheet" href="<?php echo base_url();?>css/style_admin_login.css">
+
+<div id="wrapper_admin">
+
+	<div id="login_box">
+
+		<form action = "<?php echo base_url();?>index.php/admin_account/admin_login" method = "post">
+
+			<div class="login_container">
+				<input class="login_input" type = "text" name="username" placeholder="username" size="27"/>
+			</div>
+
+			<div class="login_container">
+				<input class="login_input" type = "password" name="password" placeholder="password" size="27"/>
+			</div>
+
+			<div class="login_container" id="submit_box">
+				<input type = "submit" name="submit" value="Login" id="submit_button" />
+			</div>
+
+		</form>
+
+	</div>
+
+</div>
