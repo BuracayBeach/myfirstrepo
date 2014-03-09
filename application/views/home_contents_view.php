57547eaaefafccbf89927ae25366dca4a9cbd2f1
Desery Sabado <deserysabado@gmail.com>
2014-03-09 22:48:53 +0800

    home view

---
+++ application/views/home_contents_view.php
@@ -0,0 +1,10 @@
+<div id="home_content_container" div class="hideable">
+	
+<img src="images/home_image.jpg">
+<h3>Com Lib ICS Library System</h3>
+
+<p>
+	The ICS Library System is designed to make the services offered by the ICS library more accessible and convenient by putting them available online. The system aims to automate the process of borrowing and lending of books from the library and as well as to manage and record the books and the borrowers. The users (students and faculty members) can search, view and borrow books given that they have logged in and the administrator (librarian) may update the status of the books to either available or borrowed, add/delete/disable/enable a user account.
+</p>
+
+</div>
