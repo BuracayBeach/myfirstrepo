ced64df07f22f807a9582096c595f9ca94c804e5
KJCA <kimjoshuaadvincula@gmail.com>
2014-03-10 23:19:39 +0800

    changed logo on googlemode

--- application/views/search_google_view.php
+++ application/views/search_google_view.php
@@ -13,7 +13,7 @@
 	<form method="POST" action="<?php echo base_url()?>ihome">
 
 		<div id="bg"><img src="<?php echo base_url();?>images/chibi1.png" id="chibi1"/><img src="<?php echo base_url();?>images/chibi2.png" id="chibi2"/></div>
-		<div id="logcon"><img src="<?php echo base_url();?>images/logo_full_min.png" id="logoogle"></div>
+		<div id="logcon"><img src="<?php echo base_url();?>images/logo_full3_ws_min.png" id="logoogle"></div>
 		<div class="form-group" id="google_bg"><input class="form-control" id="google_search_text" type="search" name='home_search_text' autofocus='true' placeholder='Keywords...' maxlength='99'  />
 		<br/><input class="btn btn-primary" id='google_submit_search' type="submit" name="submit_search" value="Search"/>
 		</div>
