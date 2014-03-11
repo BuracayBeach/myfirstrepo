3a4ac209489a4b38d30221b6e036aec9f03155e0
Allan Conda <allan.conda@gmail.com>
2014-03-11 11:28:12 +0800

    Merge branch 'FTP_v1' into merge/ftp-master
    Conflicts:
    	application/controllers/announcement.php
    	application/controllers/book.php
    	application/controllers/home.php
    	application/views/admin_login_view.php
    	application/views/header.php

---
+++ application/about_us.php
@@ -0,0 +1,105 @@
+<div id="about_us_wrapper">
+	<div class="tabbable tabs-top">
+		<div class="bs-docs-example">
+        <ul id="myTab" class="nav nav-tabs">
+          <li class="active"><a href="#icslib" data-toggle="tab">ICS Library System</a></li>
+          <li><a href="#history" data-toggle="tab">History</a></li>
+          <li><a href="#missionvision" data-toggle="tab">Mission and Vision</a></li>
+		  <li><a href="#adminprofile" data-toggle="tab">Administrator Profile</a></li>
+		  <li><a href="#developers" data-toggle="tab">Developers</a></li>
+        </ul>
+        <div id="myTabContent" class="tab-content">
+          <div class="tab-pane fade in active" id="icslib">
+            <p>The ICS Library System is designed to make the services offered by the ICS library more accessible and convenient by putting them available online. The system aims to automate the process of borrowing and lending of books from the library and as well as to manage and record the books and the borrowers. The users (students and faculty members) can search, view and borrow books given that they have logged in and the administrator (librarian) may update the status of the books to either available or borrowed, add/delete/disable/enable a user account.</p>
+			</div>
+          <div class="tab-pane fade" id="history">
+            <p>On January 26, 1995, the Institute of Computer Science was established by the UP Board of Regents. ICS is the system institute for computer sciences of the whole UP system.
+On the same date, the ICS library, which was back then just a reading room, was also opened to cater the needs of Computer Science major.
+It started keeping Thesis and Special Problem manuscripts. Little by little, books on related topics from around the world were added to the library’s collection. As years pass, more books and publications were added. 
+Adding, updating and keeping track of the books, borrowing and reservation processes, and other services offered were all done manually. It was simple in the beginning, but as the number of books increases, the harder it is to do all services. 
+Finally on March 14, 2014, the ComLib: ICS Library System was finished to replace the tedious processes on services offered by the ICS Library. It is a web-based application where students create accounts to reserve and borrows books and made all library services easier to do. 
+At present, the ICS library continues to add more materials to help more students not only in UPLB but also to those who has the interest and passion to the field.
+</p>
+          </div>
+          <div class="tab-pane fade" id="missionvision">
+
+            <p><br>
+			<b>Mission</b>
+			<br>
+			<br>
+			It is the mission of the ICS Library to provide easier and free access to comprehensive resources for education, research and reference. This includes faster delivery of information, lifetime accessibility of materials and the development of innovative library practices and services.
+			<br>
+			<br>
+			<b>Vision</b>
+			<br>
+			<br>
+			We aspire to provide people a means of accessing and sharing information easier. Through the ICS Library System, we believe that we can provide a wide variety of resources and other intellectual materials that will help fulfil the educational needs of the community.
+			</p>
+			</div>
+		  <div class="tab-pane fade" id="adminprofile">
+            <p><br>
+            <b>Name:</b> Marites M. Gironella
+			<br><b>Position:</b> Administrative Staff at ICS, UPLB
+			<br><b>Started working in ICS:</b> 2010
+			<br><b>Civil Status:</b> Married
+			<br><b>Date of Birth:</b> July 10, 1979
+			<br><b>Place of Birth:</b> Los Banos, Laguna
+			<br><b>Graduated from:</b> Systems Technology Institute (STI College) - Class of 1999
+			<br>Caloocan, Philippines
+			<br>Bachelor of Science in Computer Science
+			<br>Liceo de Los Banos - Class of 1996
+			<br>Los Banos, Laguna, Philippines
+			<br><b>Home Address:</b> #2321 Tuntungin-Putho, Los Banos, Laguna
+			<br><b>Contact Number:</b> (+63) 921 293 3415 / 563-2313
+			<br><b>Email Address:</b> mmgironella@gmail.com
+			<br><b>Work Hours:</b> M-F: 8am-12nn, 1pm-5pm
+				</p>
+			</div>
+		  <div class="tab-pane fade" id="developers">
+            <p><br>
+            <b>Project Manager:</b> Sabado, Desery
+			<br>
+			<br><b>Team 1 Team Lead:</b> Conda, Anthony Allan
+			<br><b>Developers:</b>
+			<br>1.	Merced, Bianca Patricia
+			<br>2.	Cosuco, Raphael 
+			<br>3.	Lago, Lailani
+			<br>
+			<br><b>Team 2 Team Lead:</b> Bernal, Kevin Lloyd
+			<br><b>Developers:</b>
+			<br>1.	De Ramos, Marc Ulysis
+			<br>2.	Villanueva, Yssa Marie
+			<br>3.	Advincula, Kim Joshua
+			<br>
+			<br><b>Team 3 Team Lead:</b> Castueras, Carl Adrian
+			<br><b>Developers:</b>
+			<br>1.	Padilla, Edzer Josh
+			<br>2.	Bravo, Cyril Justine
+			<br>3.	Zarate, Billy Joel Arlo
+			<br>
+			<br><b>Team 4 Team Lead:</b> Benedicto, Rey
+			<br><b>Developers:</b>
+			<br>1.	Angeles, Alyssa Jean
+			<br>2.	Medina, Maila Angelica
+			<br>3.	Sabado, Desery
+			<br>4.	Cuenca, Miguel Anton Paulo
+			<br>
+			<br><b>Team 5 Team Lead:</b> Vita, Christopher Jade
+			<br><b>Developers:</b>
+			<br>1.	Castaneda, Inna Alexandria
+			<br>2.	Aldave, Carlo
+			<br>3.	Mendoza, Arlene
+			<br>4.	Tuyo, Lovely Joy
+			<br>
+			<br><b>Team UI Team Lead:</b> Advincula, Kim Joshua
+			<br><b>Developers:</b>
+			<br>1.	Bernal, Kevin Lloyd
+			<br>2.	Merced, Bianca Patricia
+			<br>3.	Sabado, Desery
+			</p>
+			</div>
+   
+        </div>
+      </div>
+	</div>
+</div>
--- js/announcements_table_generator.js
+++ js/announcements_table_generator.js
@@ -5,6 +5,7 @@ function generateAnnouncementsTable(isAdmin){
         try{
             data = JSON.parse(data);
 
+
             data.forEach(function(entry){
                 generateAnnouncementRow(entry,isAdmin);
             });
@@ -56,47 +57,12 @@ function generateAnnouncementRow(data,isAdmin){
     carouselInner.append(itemHTML);
     if(len == 0) carouselInner.closest('.carousel').show();
 
-/*
-    var buttons = "";
-    var editable = "";
-    var prevDataInputs = "";
-    var editButtons = "";
-    if(isAdmin){
-        prevDataInputs = '<h4 hidden class="prev_question"></h4>' +
-            '<textarea style="display:none;" id="answer_'+data.id+'" class="answer_editor"></textarea>';
-        buttons = '<button class="save_faq_button" style="display:none;">Save</button>' +
-            '<button class="cancel_faq_button" style="display:none;">Cancel</button>';
-        editButtons = '<button class="edit_announcement_button">Edit</button>'+
-            '<button class="delete_announcement_button">Delete</button>';
-        editable = 'contenteditable="false"';
-    }
-    var rowHTML = '<tr announcement_id="'+data.announcement_id+'" class="announcement_table_row">'+
-        '<td class="announcement_table_data">'+
-        '<h4 '+editable+' class="announcement_title">'+data.announcement_title+'</h4>'+
-        '<div class="sub-heading small-font">posted on <span class="date_posted">'+fd.toDateString() +'</span> by '+
-        '<span class="announcement_author">'+data.announcement_author+'</span></div>' +
-        '<div class="announcement_content_container"><p class="announcement_content">'+data.announcement_content+'</p></div>'+
-        editButtons +
-        '</td>'+
-        '</tr>';
-
-
-    var tableContainer = $('#announcements_table_container');
-    if(tableContainer.find('table').length == 0){
-        tableContainer.append(tableHTML);
-        tableContainer.find('table').append($('<tbody>'));
-    }
-    tableContainer.find('table').find('tbody:last').append(rowHTML);
-
-    var firstTr = tableContainer.find('table tbody tr').first();
-    if(firstTr.find('#add_faq_container').length == 0 ){
-        tableContainer.find('table tbody').prepend(rowHTML);
-    }else{
-        tableContainer.find('table').find('tbody tr:first').after(rowHTML);
-    }*/
 }
 
 $('announcement_manage_container').ready(function(){
     var isAdmin = $('#announcement_manage_container').length == 1;
+    $('#announcements_container').hide();
     generateAnnouncementsTable(isAdmin);
+    $('#announcements_container').fadeIn();
+
 });
